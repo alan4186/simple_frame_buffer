a02d04d3dbbac5bb0343d32af3264f01bd4f5dc1
unknown <ehretaj@CEC-EGB267-8.it.muohio.edu>
2016-05-01 00:01:55 -0400

    created filter to overwrite image data

--- image_processing.v
+++ image_processing.v
@@ -2,68 +2,29 @@ module image_processing(
   input CLOCK_50,
   input RESET,
   
-  input [7:0] IM_DATA, OBJ_DATA, 
+  input [7:0] IM_DATA, 
   input [9:0] OBJ_X, OBJ_Y,
-  input [18:0] IM_ADDR, OBJ_ADDR,
+  input [18:0] FB_ADDR,
   
   output [7:0] FB_DATA,
-  output [18:0] FB_ADDR,
-  
   
   );
   
-  wire cs_rdempty, os_rdempty,
-  wire fifo_rst;
   
+  reg [7:0] overwrite
+  reg [18:0] obj_origin
   
+  assign obj_origin = OBJ_X + (OBJ_Y * 640);
   
-   
+  assign overwrite[0] = (FB_ADDR == obj_origin[18:3] + (640*0)) ? 1'b1 : 1'b0;
+  assign overwrite[1] = (FB_ADDR == obj_origin[18:3] + (640*1)) ? 1'b1 : 1'b0;
+  assign overwrite[2] = (FB_ADDR == obj_origin[18:3] + (640*2)) ? 1'b1 : 1'b0;
+  assign overwrite[3] = (FB_ADDR == obj_origin[18:3] + (640*3)) ? 1'b1 : 1'b0;
+  assign overwrite[4] = (FB_ADDR == obj_origin[18:3] + (640*4)) ? 1'b1 : 1'b0;
+  assign overwrite[5] = (FB_ADDR == obj_origin[18:3] + (640*5)) ? 1'b1 : 1'b0;
+  assign overwrite[6] = (FB_ADDR == obj_origin[18:3] + (640*6)) ? 1'b1 : 1'b0;
+  assign overwrite[7] = (FB_ADDR == obj_origin[18:3] + (640*7)) ? 1'b1 : 1'b0;
   
-  assign FB_ADDR = OBJ_X + (OBJ_Y * 640);
+  assign FB_DATA = (overwrite > 8'h00) ? 8'hff : IM_DATA;
   
-  video_stream_fifo composite_stream(
-    .aclr(fifo_rst),
-    .data({IM_DATA, IM_ADDR}),
-    .rdclk,
-    .rdreq,
-    .wrclk,
-    .wrreq,
-    .q,
-    .rdempty(cs_rdempty),
-    .wrfull);
-    
-  video_stream_fifo objec_stream(
-    .aclr(fifo_rst),
-    .data({OBJ_DATA, OBJ_ADDR}),
-    .rdclk,
-    .rdreq,
-    .wrclk,
-    .wrreq,
-    .q,
-    .rdempty(os_rdempty),
-    .wrfull);
-  
-  always@(posedge CLOCK_50 or negedge RESET) begin
-    if(RESET == 1'b0) begin
-	   fifo_rst <= 1'b0;
-	 end else begin
-     fifo_rst <= 1'b1;
-   
-	 
-	 
-	 
-	 end
-  end
-  
-  always@(posedge CLOCK_RFIFO or negedge RESET) begin
-    if (RESET == 1'b0) begin
-      stream_state <= 1'b0; 
-    else end begin
-      if(os_rdempty) begin
-        stream_state <= 1'b0; // stream the composite video signal
-      end else begin
-        stream_state <= !stream_state;
-      end
-    end // rst if
-  end // always
-  
+endmodule
